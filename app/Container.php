4f78eb6c8a2ca2e9cbd69e8c2cb5814b163cb4aa
Roger Forner Fabre <hello@rogerforner.com>
2018-05-10 17:27:51 +0200

    menú lateral + contenidors

--- app/Container.php
+++ app/Container.php
@@ -27,12 +27,36 @@ class Container extends Model
      * Eloquent: Relationships.
      **************************************************************************/
     /**
+     * Parent Container.
+     * Get the parent container of the container.
+     * 
+     * The method name "parent()" is translated to 'parent_id', for this reason
+     * we passed 'container_parent_id' in the second parameter.
+     * 
+     * If the FK had been called 'parent_id', the second parameter would not have
+     * been necessary.
+     */
+    public function parent()
+    {
+        return $this->belongsTo(Container::class, 'container_parent_id');
+    }
+
+    /**
+     * Containers.
+     * Get the containers of the container (parent).
+     */
+    public function containers()
+    {
+        return $this->hasMany(Container::class, 'container_parent_id');
+    }
+
+    /**
      * User.
      * Get the user of the container.
      */
     public function user()
     {
-        return $this->belongsTo(User::class)->withDefault();
+        return $this->belongsTo(User::class);
     }
 
     /**
@@ -41,7 +65,7 @@ class Container extends Model
      */
     public function vehicle()
     {
-        return $this->belongsTo(Vehicle::class)->withDefault();
+        return $this->belongsTo(Vehicle::class);
     }
 
     /**
