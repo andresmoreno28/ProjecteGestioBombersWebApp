071765697cd3bee2d1ddd6ae7c099b52efa6ad32
AMoreno <andresmrubio28@gmail.com>
2018-05-14 16:14:55 +0200

    commit

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Barryvdh\DomPDF\Facade as PDF;
+use Barryvdh\Snappy\Facades\SnappyPdf as SPDF;
 use App\Region;
 use App\User;
 use App\Vehicle;
 use App\Container;
 use App\Material;
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use dompdf\dompdf\src\Adapter\CPDF;
+use dompdf\dompdf\src\Exception;
 
 class PDFController extends Controller
 {
@@ -26,8 +31,42 @@ class PDFController extends Controller
     public function crearPDF()
     {
       
+        
+        $data = $this->getData();
+        
+        $view =  \View::make('informes.report', $data)->render();
+        //$data = array('region' => $region,'user' => $user, 'vehicle' => $vehicle,'contenidors' => $contenidors);
+
+        $pdf = PDF::loadHTML($view);
+        return $pdf->download('informe');
+
+        //$snappy = SPDF::generate('http://www.w3schools.com', '/tmp/testPdf.pdf');
+
+        /*return view('informes.report', ['region' => $region,'user' => $user, 'vehicle' => $vehicle,'contenidors' => $contenidors]);*/
+
+        /*$options = new Dompdf\Options();
+        $options->setDpi(150);
+        $pdf = new \Dompdf($options);
+        $pdf ->loadView('informes.report', $data)->save('informe.pdf');
+        $pdf->render();
+        return $pdf->stream('informe.pdf');*/
+
+        // instantiate and use the dompdf class
+       // $options = new Options();
+        //$options->setDpi(150);
+        //$dompdf = PDF::loadView('informes.report', $data);
+        //$dompdf->setOptions($options);
+        // (Optional) Setup the paper size and orientation
+        //$dompdf->setPaper('A4', 'portrait');
+
+        // Output the generated PDF to Browser
+        //$dompdf->stream('informe.pdf');
+    }
+
+    public function getData()
+    {
         //Agafem el vehicle mitjançant el seu "id"
-        $vehicle = Vehicle::findOrFail(1);
+        $vehicle = Vehicle::find(1);
 
         //Agafem l'usuari a través de la seva relació amb el vehicle
         $user = $vehicle->user;
@@ -36,10 +75,14 @@ class PDFController extends Controller
         $region = $user->region;      
 
         $contenidors = $vehicle->containers;
-       
-        return view('informes.report', ['region' => $region,'user' => $user, 'vehicle' => $vehicle,'contenidors' => $contenidors]);
 
-        $pdf = PDF::loadView('informes/report');
-        return $pdf->download('informe.pdf');
+        $data = [
+            'region' => $region,
+            'user' => $user,
+            'vehicle' => $vehicle,
+            'contenidors' => $contenidors
+        ];
+
+        return $data;
     }
 }
--- resources/views/informes/report.blade.php
+++ resources/views/informes/report.blade.php
@@ -9,27 +9,114 @@
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-	<link rel="stylesheet" href="{{ asset('css/report.css') }}">
+	<style type="text/css">
+			body {
+		margin: 0 auto;
+		width: 90%;
+	}
+
+	#header {
+		height: 100px;
+		 border: 1px solid red; 
+		top: 0px;
+		left: 0px;
+		right: 0px;
+		margin: 30px;
+		overflow: auto;
+	}
+
+	#eo {
+		margin-top: 50px;
+		 border:2px solid green; 
+		width: 100%;
+	}
+
+	#semicontainer1 {
+		width: 20%;
+		border: 2px solid blue;
+		float: left;
+	}
+
+	#semicontainer2 {
+		width: 79%;
+		border: 2px solid green;
+		float: right;
+	}
+
+	#inventari {
+		width: 11.1%;
+		border: 3px solid black;
+		/*margin-top: -60px;*/
+		/*margin-left: 200px;*/
+		font-size: 22px;
+		/*position: absolute;*/
+		text-align: center;
+		border-collapse: collapse;
+		float: right;
+	}
+
+	#regio_parc_vehicle {
+		width: 11.1%;
+		border: 3px solid black;
+		/*margin-top: -60px;*/
+		/*margin-left: 300px;*/
+		font-size: 22px;
+		position: absolute;
+		text-align: center;
+		border-collapse: collapse;
+		float: right;
+	}
+
+	#data {
+		width: 11.1%;
+		border: 3px solid black;
+		/*margin-top: -60px;*/
+		/*margin-left: 400px;*/
+		font-size: 22px;
+		position: absolute;
+		text-align: center;
+		border-collapse: collapse;
+		float: right;
+	}
+
+	.celles_a_centrar {
+		text-align: center;
+	}
+
+	#contorn_contenidors {
+		text-align: center;
+		border: 3px solid black;
+		border-collapse: collapse
+		margin-top: 20px;
+		margin-bottom: 20px;
+		margin-left: 10px;
+		padding-left: 20px;
+		width: 30%;
+		page-break-inside: auto;
+	}
+
+</style>
 </head>
 <body>
-	<div class="row">
-		<div id="header" class="container-fluid">
+	<div id="header" class="container-fluid">
+		<div id="semicontainer1">
 			<img src="img/logo.jpg" height="90" width="165">
-			<div id="inventari" class="container-fluid">INVENTARI</div>
-			<div id="regio_parc_vehicle" class="container-fluid">{{$region->codi}}.{{$user->codi_parc}}.{{$vehicle->codi}}</div>
+		</div>
+		<div id="semicontainer2">
+			<div id="inventari">INVENTARI</div>
+			<div id="regio_parc_vehicle">{{$region->codi}}.{{$user->codi_parc}}.{{$vehicle->codi}}</div>
 
 			@php 
 				$ldate = new DateTime('today');
 				$ldate = date('d-m-Y')
 			@endphp
 
-			<div id="data" class="container-fluid"> {{ $ldate }}</div>
+			<div id="data"> {{ $ldate }}</div>
 		</div>
 	</div>
-	<div class="row">
-		<div id="eo" lass="container-fluid"> 
+		<div id="eo" class="container-fluid"> 
 
 		@foreach ($contenidors as $contenidor)
 			<div id="contorn_contenidors" class="row"> 
@@ -77,7 +164,6 @@
 				</div> 
 		@endforeach
 		</div>
-	</div>
 
 </body>
 </html>
