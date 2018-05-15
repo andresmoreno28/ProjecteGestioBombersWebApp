e82f43ee177ee3d2579166285ffe45bf4b798f36
enricbc <33874389+enricbc@users.noreply.github.com>
2018-05-15 15:05:20 +0200

    321

--- database/migrations/2018_05_04_101706_create_materials_table.php
+++ database/migrations/2018_05_04_101706_create_materials_table.php
@@ -15,7 +15,7 @@ class CreateMaterialsTable extends Migration
     {
         Schema::create('materials', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('referencia')->unique()->nullable();
+            $table->string('referencia')->nullable();
             $table->string('nom');
             $table->unsignedInteger('quantitat_prevista')->default(0);
             $table->unsignedInteger('quantitat')->default(0);
